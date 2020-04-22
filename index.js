412dc49e7bb8f8179075e2edf69c02aff5b37538
Lars Funke <lars.funke@fqsmedia.de>
2020-04-22 11:10:06 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,229 @@
+const mysql = require('mysql');
+const express = require('express');
+const session = require('express-session');
+const cookieSession = require('cookie-session')
+const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+const path = require('path');
+const argon2 = require('argon2');
+const fs = require('fs');
+const dirTree = require("directory-tree");
+const winston = require('winston');
+const child = require('child_process');
+const sanitize = require("sanitize-filename");
+
+var mail = require('sendmail')({
+	devHost:'localhost',
+	devPort: 25,
+	silent: true
+});
+
+var dbconfig = {
+	host     : 'localhost',
+	user     : 'rehearsal',
+	password : 'sgvUV4aJhJM1V0vd',
+	database : 'musicollective'
+};
+
+
+var ulogger = winston.createLogger({
+	format: winston.format.combine(
+	    winston.format.timestamp(),
+	    winston.format.json()
+	),
+	transports: [
+		new winston.transports.File({ filename: 'log/users.log'}),
+	]
+});
+
+var fblogger = winston.createLogger({
+	format: winston.format.combine(
+	    winston.format.timestamp(),
+	    winston.format.json()
+	),
+	transports: [
+		new winston.transports.File({ filename: 'log/feedback.log'}),
+	]
+});
+
+var jserrorlogger = winston.createLogger({
+	format: winston.format.combine(
+	    winston.format.timestamp(),
+	    winston.format.json()
+	),
+	transports: [
+		new winston.transports.File({ filename: 'log/jserrors.log'}),
+	]
+});
+
+var dbc;
+function handleDisconnect() {
+    dbc = mysql.createConnection(dbconfig);
+    dbc.connect( function onConnect(err) {
+        if (err) {
+            console.log('Error when connecting to database:', err);
+            setTimeout(handleDisconnect, 10000);
+        }
+    });
+
+    dbc.on('error', function onError(err) {
+        console.log('Database error. Reconnecting.', err);
+        if (err.code == 'PROTOCOL_CONNECTION_LOST' || err.code == 'ECONNRESET') {
+            handleDisconnect();
+        } else {
+            throw err;
+        }
+    });
+}
+handleDisconnect();
+
+var app = express();
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.use("/songs", express.static(path.join(__dirname, '/songs')));
+app.use(express.static(path.join(__dirname, '/static')));
+app.use("/jquery", express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.use("/bootstrap", express.static(path.join(__dirname, '/node_modules/bootstrap/dist')));
+app.use("/fa", express.static(path.join(__dirname, '/node_modules/font-awesome')));
+app.use("/md", express.static(path.join(__dirname, '/node_modules/markdown-it/dist')));
+app.use("/bowser", express.static(path.join(__dirname, '/node_modules/bowser')));
+app.use("/opus-recorder", express.static(path.join(__dirname, '/node_modules/opus-recorder/dist')));
+app.use("/waveform-playlist", express.static(path.join(__dirname, '/waveform-playlist-fixed/dist/waveform-playlist')));
+
+
+/*app.use(session({
+	secret: 'wrong horse battery staple lol',
+	resave: true,
+	saveUninitialized: true
+}));*/
+
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['wrong horse battery staple lol'],
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
+
+var rawparser = bodyParser.raw({limit: '1000mb', type: "application/octet-stream"});
+var urlencoded = bodyParser.urlencoded({extended : true});
+var json = bodyParser.json();
+
+app.get('/', function(req, res) {
+	if (req.session.loggedin) {
+		if(req.session.username != "lars.funke")
+		ulogger.log('info', 'Page loaded by ' + req.session.username + ', ip ' + req.headers['x-forwarded-for']);
+		//TODO: Optimize this!
+		var tree = dirTree("songs");
+		var data = {songs: null};
+		data.songs = tree.children.map((a) => ({
+			name: a.name, 
+			audio: a.children.filter(a => a.name != "Text.md").map(a => ({src: a.path, name: a.name.split('.')[0]})),
+			text: a.children.filter(a => a.name == "Text.md").map(a => a.path)[0]
+		}));
+		res.render('player', {
+			session: req.session, 
+			song: tree.children.map(a => a.name), 
+			resources: JSON.stringify(data)
+		});
+	} else {
+		var err = '';
+		if(req.session.loginError) {
+			err = 'Login fehlgeschlagen.';
+			req.session.loginError = false;
+		}
+		res.render('login', {error: err});
+	}
+	//response.end();
+});
+
+app.get('/deauth', function(req, res){
+	req.session = null;
+	res.redirect('/');
+});
+
+app.post('/auth', urlencoded, json, async function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username == "demo" && password == "demo") {
+		request.session.loggedin = true;
+		request.session.username = "demo";
+		request.session.firstname = "Demo User";
+		return response.redirect('/');
+	}
+
+	if (username && password) {
+		dbc.query('SELECT first_name, last_name, passwd, mail FROM members WHERE username = ? ', [username], async function(error, results, fields) {
+			if(error)
+				console.error(error);
+
+			if (results.length > 0) {
+				var correct = await argon2.verify(results[0]['passwd'], password).catch((err) => { console.log(err); });;
+				if(correct) {
+					request.session.loggedin = true;
+					request.session.username = username;
+					request.session.firstname = results[0]['first_name'];
+					request.session.lastname = results[0]['last_name'];
+					request.session.mail = results[0]['mail'];
+				} else {
+					request.session.loginError = true;
+				}
+			} else {
+				request.session.loginError = true;
+			}
+			ulogger.log('warn', (request.session.loggedin?'':'Failed ') + 'login by ' + username + ', ip ' + request.headers['x-forwarded-for']);
+			response.redirect('/');
+		});
+	} else {
+		response.redirect('/');
+	}
+	
+});
+
+app.post('/feedback', urlencoded, json, function(req, res) {
+	mail({
+       from: req.session.firstname + ' ' + req.session.lastname + ' <' + req.session.mail + '>' || 'noone@musicollective.de',
+       to: 'Lars Funke <lars@musicollective.de>',
+       subject: '[Digitale Probe] Feedback von ' + req.session.firstname,
+       html: req.body.msg + '<br /><br />' + req.body.ua + '<br /><br />' + JSON.parse(req.body.con).join("<br />")
+   	},
+   	function(err,response){
+      if(err){
+         console.log("Mail error:" + err);
+      }
+	});
+	fblogger.log('info', req.session.username + ': ' + req.body.msg + ' ' + req.body.ua)
+	res.json('success');
+});
+
+app.post('/jserror', urlencoded, json, function(req, res) {
+	req.body.data.user = req.session.username;
+	jserrorlogger.log('error', req.body.data);
+	res.json('ok');
+});
+
+app.post('/upload', rawparser, function(req, res) {
+	var ar = parseInt(req.header('X-Samplerate')) || 44100;
+	var song = req.header('X-Song') || '';
+	var proc = child.spawn('ffmpeg', [
+	  '-f', 'f32le',
+	  '-ar', ar,
+	  '-i', '-',
+	  '-c:a', 'flac',
+	  '-compression_level', '11',
+	  'uploads/'+ (new Date).toISOString() + ' ' + req.session.username + ' - ' + sanitize(song) +'.flac'
+	]);
+
+	proc.stdin.write(req.body);
+	proc.stdin.end();
+
+	proc.on('close', (code) => {
+	  if(code == 0)
+	  	res.json("ok");
+	  else
+	  	res.json("not ok");
+	});
+});
+
+app.listen(3000);
---
+++ static/player.js
@@ -0,0 +1,286 @@
+var userMediaStream;
+var playlist;
+var constraints = { 
+  audio: true, 
+  noiseSuppresion: false, 
+  echoCancellation: false, 
+  autoGainControl: true,
+  latency: 0,
+  sampleSize: 16
+};
+var loadingPromise;
+
+navigator.getUserMedia = (navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia ||
+  navigator.msGetUserMedia);
+
+function gotStream(stream) {
+  userMediaStream = stream;
+  playlist.initRecorder(userMediaStream);
+  startRecording();
+}
+
+function initializeRecording() {
+  if (navigator.mediaDevices) {
+    navigator.mediaDevices.getUserMedia(constraints)
+    .then(gotStream)
+    .catch(console.error);
+  } else if (navigator.getUserMedia && 'MediaRecorder' in window) {
+    navigator.getUserMedia(
+      constraints,
+      gotStream,
+      console.error
+    );
+  }
+}
+
+function startRecording() {
+  if(isRecording) return;
+  isRecording = true;
+  $(".btn-record").addClass("active");
+  $(".btn-play").addClass("disabled");
+  $(".btn-pause").addClass("disabled");
+  $(".btn-rewind").addClass("disabled");
+  ee.emit("record");
+}
+
+/*
+ * This script is provided to give an example how the playlist can be controlled using the event emitter.
+ * This enables projects to create/control the useability of the project.
+*/
+var ee = playlist.getEventEmitter();
+ee.emit("automaticscroll", true);
+var $container = $("body");
+var $timeFormat = $container.find('.time-format');
+var $audioStart = $container.find('.audio-start');
+var $audioEnd = $container.find('.audio-end');
+var $time = $container.find('.audio-pos');
+var $select = $container.find('.select-songs')
+
+var format = "hh:mm:ss.uuu";
+var startTime = 0;
+var endTime = 0;
+var audioPos = 0;
+var downloadUrl = undefined;
+var isLooping = false;
+var isRecording = false;
+var playoutPromises;
+var loadingFinished;
+var loadingTotal;
+var uploadTrack;
+
+function toggleActive(node) {
+  var active = node.parentNode.querySelectorAll('.active');
+  var i = 0, len = active.length;
+
+  for (; i < len; i++) {
+    active[i].classList.remove('active');
+  }
+
+  node.classList.toggle('active');
+}
+
+function cueFormatters(format) {
+
+  function clockFormat(seconds, decimals) {
+    var hours,
+        minutes,
+        secs,
+        result;
+
+    hours = parseInt(seconds / 3600, 10) % 24;
+    minutes = parseInt(seconds / 60, 10) % 60;
+    secs = seconds % 60;
+    secs = secs.toFixed(decimals);
+
+    result = (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (secs < 10 ? "0" + secs : secs);
+
+    return result;
+  }
+
+  var formats = {
+    "seconds": function (seconds) {
+        return seconds.toFixed(0);
+    },
+    "thousandths": function (seconds) {
+        return seconds.toFixed(3);
+    },
+    "hh:mm:ss": function (seconds) {
+        return clockFormat(seconds, 0);   
+    },
+    "hh:mm:ss.u": function (seconds) {
+        return clockFormat(seconds, 1);   
+    },
+    "hh:mm:ss.uu": function (seconds) {
+        return clockFormat(seconds, 2);   
+    },
+    "hh:mm:ss.uuu": function (seconds) {
+        return clockFormat(seconds, 3);   
+    }
+  };
+
+  return formats[format];
+}
+
+function updateSelect(start, end) {
+  if (start < end) {
+    $('.btn-trim-audio').removeClass('disabled');
+    $('.btn-loop').removeClass('disabled');
+  }
+  else {
+    $('.btn-trim-audio').addClass('disabled');
+    $('.btn-loop').addClass('disabled');
+  }
+
+  $audioStart.val(cueFormatters(format)(start));
+  $audioEnd.val(cueFormatters(format)(end));
+
+  startTime = start;
+  endTime = end;
+}
+
+function updateTime(time) {
+  $time.html(cueFormatters(format)(time));
+
+  audioPos = time;
+}
+
+updateSelect(startTime, endTime);
+updateTime(audioPos);
+
+/*
+* Code below sets up events to send messages to the playlist.
+*/
+
+$container.on("click", ".btn-play", function() {
+  if(!isRecording)
+    ee.emit("play");
+});
+
+$container.on("click", ".btn-pause", function() {
+  if(!isRecording) {
+    isLooping = false;
+    ee.emit("pause");
+  }
+});
+
+$container.on("click", ".btn-stop", function() {
+  isLooping = false;
+  ee.emit("stop");
+});
+
+$container.on("click", ".btn-rewind", function() {
+  if(!isRecording) {
+    isLooping = false;
+    ee.emit("rewind");
+  }
+});
+
+$container.on("click", ".btn-fast-forward", function() {
+  isLooping = false;
+  ee.emit("fastforward");
+});
+
+$container.on("click", ".btn-clear", function() {
+  isLooping = false;
+  ee.emit("clear");
+});
+
+$container.on("click", ".btn-record", function() {
+  if(userMediaStream == null || !userMediaStream.active)
+    initializeRecording();
+  else {
+    startRecording();
+  }
+});
+
+function loadnew(song) {
+  ee.emit("clear");
+  ee.emit("stop");
+  ee.emit("rewind");
+  $("#lyrics-container").hide();
+  $("#lyrics").empty();
+  if (song != -1) {
+    var list = resources.songs[song].audio;
+    loadingPromise = playlist.load(list).then(function(){
+      $("#lyrics-container").height($("#playlist").height());
+      $("#lyrics-container").fadeIn();
+      $("#loading-progress").animate({ opacity: 0 }, function(){
+        $("#loading-progress > .progress-bar").width("0%");
+      });
+    });
+    loadingTotal = list.length * 3; //3 states to run through
+    loadingFinished = 0;
+    $("#loading-progress").animate({ opacity: 1 });
+    $("#loading-progress > .progress-bar").text("0%");
+    $("#loading-progress > .progress-bar").width("0%");
+
+    if(resources.songs[song].text != null) {
+        $.get(resources.songs[song].text, function( data ) {
+          $("#lyrics" ).html( md.render(data) );
+        });
+      }
+    return;
+  }
+  loadingPromise = new Promise((resolve) => { resolve(); });
+}
+
+$select.on("change", function() {
+  var song = parseInt($select.val());
+  if(loadingPromise != null) {
+    loadingPromise.then(() => loadnew(song))
+  } else {
+    loadnew(song);
+  }
+});
+
+
+/*
+* Code below receives updates from the playlist.
+*/
+ee.on("select", updateSelect);
+
+ee.on("timeupdate", updateTime);
+
+ee.on("audiorequeststatechange", function(state, src) {
+  var percent = (++loadingFinished / loadingTotal * 100).toFixed() + "%";
+  $("#loading-progress > .progress-bar").text(percent);
+  $("#loading-progress > .progress-bar").width(percent);
+});
+
+ee.on("stop", function(){
+  isRecording = false;
+  $(".btn-record").removeClass("active");
+  $(".btn-play").removeClass("disabled");
+  $(".btn-rewind").removeClass("disabled");
+  $(".btn-pause").removeClass("disabled");
+});
+
+/*ee.on("loadprogress", function(percent, src) {
+  $(".loading-progress").text(percent.toFixed(0) + "%");
+});*/
+
+ee.on("upload", function(track) {
+  $("#uploadModal").modal('show');
+  uploadTrack = track;
+})
+
+
+ee.on("audiorequeststatechange", function(state, src) {
+  var name = src;
+
+  if (src instanceof File) {
+    name = src.name;
+  }
+
+  console.log("Track " + name + " is in state " + state);
+});
+
+ee.on("audiosourcesloaded", function() {
+  console.log("Tracks have all finished decoding.");
+});
+
+ee.on("audiosourcesrendered", function() {
+  console.log("Tracks have been rendered");
+});
